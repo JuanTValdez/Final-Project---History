6c316a3212a2cbed77455638e3843a8f513c9f08
JuanValdez <juantvaldez85@gmail.com>
2022-07-05 17:56:08 -0400

    formatted date picker output string

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,23 @@
-import logo from './logo.svg';
 import { useState, useEffect } from 'react';
+import Stack from '@mui/material/Stack';
+import TextField from '@mui/material/TextField';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
+import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
+import dateFormat from 'dateformat';
 import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [value, setValue] = useState(new Date('2014-08-18T21:11:54'));
+
+  const handleChange = (newValue) => {
+    setValue(newValue);
+    console.log(dateFormat(newValue, 'mm-d-yyyy'));
+    console.log(newValue);
+  };
+
   const [apiData, setApidata] = useState([]);
 
   useEffect(() => {
@@ -15,31 +29,40 @@ function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
+        {apiData.map((data, index) => {
+          return (
+            <div key={index}>
+              <h5>
+                {'Id: ' +
+                  `${data.id}` +
+                  ', Email: ' +
+                  `${data.email}` +
+                  ', Password: ' +
+                  `${data.password}`}
+              </h5>
+            </div>
+          );
+        })}
       </header>
-      {apiData.map((data, index) => {
-        return (
-          <div key={index}>
-            <h5>
-              {'Id: ' +
-                `${data.id}` +
-                ', Email: ' +
-                `${data.email}` +
-                ', Password: ' +
-                `${data.password}`}
-            </h5>
-          </div>
-        );
-      })}
+
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <Stack spacing={3}>
+          <DesktopDatePicker
+            label='Date desktop'
+            inputFormat='MM-dd-yyyy'
+            value={value}
+            onChange={handleChange}
+            renderInput={(params) => <TextField {...params} />}
+          />
+          <MobileDatePicker
+            label='Date mobile'
+            inputFormat='MM/dd/yyyy'
+            value={value}
+            onChange={handleChange}
+            renderInput={(params) => <TextField {...params} />}
+          />
+        </Stack>
+      </LocalizationProvider>
     </div>
   );
 }
